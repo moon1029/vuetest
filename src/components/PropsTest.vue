e8da39b17e16fa24fc35b481b868c789982c4895
moon1029 <xulinyu1029@126.com>
2019-11-19 18:00:05 +0800

    props，slot插槽，方法

---
+++ src/components/PropsTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <strong>Error!</strong>
+    <!-- 插槽，在父组件里面的内容渲染到子组件里面来 -->
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ["counter"],
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
